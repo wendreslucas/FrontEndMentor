b4f3604264c0487d1c047c1623137967f4633eb1
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-03-09 10:47:31 -0300

    novo ajuste no _app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,14 +4,11 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
 :root {
-  --white: black;
-  ${'' /* --white: hsl(0, 0%, 100%); */}
+  ${'' /* --white: black; */}
+  --white: hsl(0, 0%, 100%);
   --light_gray: hsl(212, 45%, 89%);
   --grayish_blue: hsl(220, 15%, 55%);
   --dark_blue: hsl(218, 44%, 22%);
-}
-
-* {
   @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap');
   box-sizing: border-box;
   font-family: 'Outfit', sans-serif;
