37c5dc5fb5a71f30903d95a35a2c09b70700bc61
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-03-09 10:51:47 -0300

    _app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,16 +9,16 @@ export const GlobalStyle = createGlobalStyle`
   --light_gray: hsl(212, 45%, 89%);
   --grayish_blue: hsl(220, 15%, 55%);
   --dark_blue: hsl(218, 44%, 22%);
-  @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap');
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+  
+ 
 }
 
 * {
 
   font-family: 'Outfit', sans-serif;
-  
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
   text-decoration: none;  
 
   @media (max-width: 1080px) {
